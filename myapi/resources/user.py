d8c7ecc01a056c1844e7a2e27edb954f7817f566
Dan Harold <dan.harold2001@hotmail.com>
2022-12-31 23:00:34 +0000

    store users lower, edit user info require auth

--- myapi/resources/user.py
+++ myapi/resources/user.py
@@ -9,16 +9,16 @@ from myapi.common.util import message, mongo_out
 @auth.verify_password
 def verify_password(username, password):
     user = db.users.find_one({"username": username})
-    if user is not None and check_password_hash(user['passwordHash'], password):
+    if user is not None and check_password_hash(user['password_hash'], password):
         return mongo_out(user)
 
 # FIELDS
 def new_user(params):
     return {
-        "username": params['username'],
+        "username": str.lower(params['username']),
         "display_username": params['display_username'],
         "email": params['email'],
-        "passwordHash": generate_password_hash(params['password']),
+        "password_hash": generate_password_hash(params['password']),
         "info": {
             "about_me": ""
         },
@@ -53,43 +53,42 @@ class UserCollection(Resource):
             return abort(400, message="Invalid params. "
                 f"Require {new_user_params}.")
         else:
-            if db.users.find_one({"username": params['username']}) is None:
+            if db.users.find_one({"username": str.lower(params['username'])}) is None:
                 db.users.insert_one(new_user(params))
-                return mongo_out(db.users.find_one({"username":params['username']})), 201
+                return mongo_out(db.users.find_one({"username":str.lower(params['username'])})), 201
             else:
-                return abort(400, message=f"User {params['username']} already exists")
+                return abort(400, message=f"User {str.lower(params['username'])} already exists")
 
 
 class User(Resource):
     """
     Handle '/api/users/<username>'
     - GET: Return user
-    - PUT: Update user info
+    - PUT: Update user info, requiring user authentication
         > Form params: [display_username, email, password_hash]
     - DELETE: Delete user
     """
 
     # return specified user
+    # TODO: ensure hashes and other private data is not publicly exposed
     def get(self, username):
         if db.users.find_one({"username":username}) is not None:
             return mongo_out(db.users.find_one({"username": username}))
         else:
             return message(f"User {username} does not exist.")
     
-    # update user info
+    # update user info - require user auth
+    @auth.login_required
     def put(self, username):
-        resp = db.users.find_one({"username": username})
-        if resp is not None:
-            for field in request.form:
-                if field in mongo_out(resp):
-                    db.users.update_one({"username":username},
-                        {'$set': {
-                            field: request.form[field]
-                        }}
-                    )
-            return mongo_out(db.users.find_one({"username":username}))
-        else:
-            return message(f"User {username} does not exist.")
+        for field in request.form:
+            if field in auth.current_user():
+                db.users.update_one(
+                    {"username": auth.current_user()['username']},
+                    {'$set': {
+                        field: request.form[field]
+                    }}
+                )
+                return mongo_out(db.users.find_one({"username":auth.current_user()['username']}))
     
     # delete user
     def delete(self, username):
