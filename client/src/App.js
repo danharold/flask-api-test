86f64da3148e59493512b896acf2b62b0e076e31
Dan Harold <dan.harold2001@hotmail.com>
2023-01-05 21:44:27 +0000

    get user data from jwt on app load

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import axios from 'axios'
+
 import Header from './components/Header';
 
 import Error404 from './pages/404';
@@ -8,24 +10,52 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 
 function App() {
-  // const [posts, setPosts] = useState([])
-  
-  // useEffect(() => {
-  //   fetch('/api/posts').then(res => res.json()).then(data => {
-  //     setPosts(posts);
-  //   });
-  // }, []);
+  const [user, setUser] = useState(()=>{
+    return undefined
+  })
 
+  console.log("RENDER APP")
+
+  useEffect(() => {
+    console.log("USEEFFECT: GET USER")
+    // decode username from jwt if available for lookup
+    try {
+      const token = document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('a'))
+      ?.split('=')[1]
+      const token_deconstructed = token.split('.');
+      const username = JSON.parse(atob(token_deconstructed[1])).username
+      axios.get('/api/users/' + username, {}, {
+        headers: {
+          "Authorization": "Bearer " + token
+        }
+      }).then((res) => {
+        setUser(res.data)
+      })
+    }
+    catch(err) {
+      console.log(err)
+    }
+  }, [])
 
   return (
     <div className="App">
         <BrowserRouter>
-          <Header/>
+          <Header user={user} />
           <Routes>
             <Route path="*" element={<Error404/>}/>
-            <Route path="/" element={<HomePage/>}/>
-            <Route path="/login" element={<LoginPage/>}/>
-            <Route path="/register" element={<RegisterPage/>}/>
+            <Route path="/" element={<HomePage user={user}/>}/>
+            {user ? 
+            <>
+              <Route path="/login" element={<HomePage user={user}/>}/>
+              <Route path="/register" element={<HomePage user={user}/>}/>
+            </> :
+            <>
+              <Route path="/login" element={<LoginPage/>}/>
+              <Route path="/register" element={<RegisterPage/>}/>
+            </>
+            }
           </Routes>
         </BrowserRouter>
     </div>
